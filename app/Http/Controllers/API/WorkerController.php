23023e9aa2a33c1405ba9a9dd9b41b35a4bf1608
WilfG <wilfriedhount@gmail.com>
2026-06-16 17:39:11 +0100

    Worker listing minor change in comment

--- app/Http/Controllers/API/WorkerController.php
+++ app/Http/Controllers/API/WorkerController.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Log;
 class WorkerController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the workers.
      */
 
 
